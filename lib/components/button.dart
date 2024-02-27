e4b5f881757b9d83b362627a18a886e57b7051fe
Mahmoud Hany <mahmoudhany374@gmail.com>
2024-02-27 17:58:07 +0200

    Profile page is uploaded

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -36,3 +36,39 @@ class LocoButton extends StatelessWidget {
     );
   }
 }
+
+//ignore: must_be_immutable
+class ProfileButton extends StatelessWidget {
+  ProfileButton({super.key,required this.buttonText,required this.onPressed});
+
+  String buttonText;
+  Function onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.AppTheme.black.withOpacity(0.1),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      height: 50,
+      width: 350,
+      child: MaterialButton(
+          onPressed: () {
+            onPressed();
+          },
+          color: Colors.AppTheme.loco,
+          textColor:Colors.AppTheme.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Text(buttonText)
+      ),
+    );
+  }
+}
--- lib/modules/profile/profile.dart
+++ lib/modules/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loco/components/button.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -6,10 +7,58 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Profile',style: TextStyle(color: Colors.black),
-      ),
-      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 50),
+            child: Row(
+              children:[
+                Image.asset('lib/resources/images/icon.png',
+                  height: 100,
+                  width: 100,
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Thomas Jefferson',
+                      style: Theme.of(context).textTheme.bodyMedium
+                    ),
+                    const SizedBox(height: 5),
+                    const Text('Thomas1@gmail.com',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                          fontFamily: 'Clash'
+                        ),
+                    ),
+                  ],
+                ),
+              ]
+            ),
+          ),
+          const SizedBox(height: 20),
+          ProfileButton(
+              buttonText: 'Personal Information',
+              onPressed: (){}
+          ),
+          const SizedBox(height: 20),
+          ProfileButton(
+              buttonText: 'History of Orders',
+              onPressed: (){}
+          ),
+          const SizedBox(height: 20),
+          ProfileButton(
+              buttonText: 'Settings',
+              onPressed: (){}
+          ),
+          const SizedBox(height: 20),
+          ProfileButton(
+              buttonText: 'Become a Brand',
+              onPressed: (){}
+          ),
+        ],
+      )
     );
   }
 }
